edbfc408347a36c909a88ecd7e2b012f8473c991
Algirdas <a.gurevicius@gmail.com>
2023-02-22 10:24:36 +0200

    initial

---
+++ src/LithuanianDates.php
@@ -0,0 +1,125 @@
+<?php
+    namespace unique\helpers;
+
+    /**
+     * Klase pateikianti metodus dirbti su datomis
+     */
+    class LithuanianDates {
+
+        protected static $months = array(
+            'ko' => array(
+                'sausio',
+                'vasario',
+                'kovo',
+                'balandžio',
+                'gegužės',
+                'birželio',
+                'liepos',
+                'rugpjūčio',
+                'rugsėjo',
+                'spalio',
+                'lapkričio',
+                'gruodžio',
+            )
+        );
+        
+        /**
+         * Gražina intervala nuo vienos datos iki kitos, neįtraukiant nereikalingu metu/mėnesių, pvz.: ( "2010-03-30", "2010-04-01" ) - grazins: "2010-03-30 - 04-01"
+         *
+         * @param string $nuo - Pirmoji data formatu ("YYYY-MM-DD")
+         * @param string $iki - Antroji data formatu ("YYYY-MM-DD")
+         * @return string
+         */
+        public static function dateRange( $nuo, $iki ) {
+
+            $nuo = date( 'Y-m-d', strtotime( $nuo ) );
+            $iki = date( 'Y-m-d', strtotime( $iki ) );
+
+            if ( $nuo === $iki ) {
+                
+                return $nuo;
+            } elseif ( date( 'Y-m', strtotime( $nuo ) ) == date( 'Y-m', strtotime( $iki ) ) ) {
+        
+                return $nuo . ' - ' . date( 'd', strtotime( $iki ) );
+            } elseif ( date( 'Y', strtotime( $nuo ) ) == date( 'Y', strtotime( $iki ) ) ) {
+        
+                return $nuo . ' - ' . date( 'm-d', strtotime( $iki ) );
+            } else {
+        
+                return $nuo . ' - ' . $iki;
+            }
+        }
+
+        /**
+         * Pilnu tekstu išvedamas datų intervalas, pvz.: "2010 m. Sausio 1 d. - 21 d."
+         *
+         * @param string $nuo - Datų intervalo pradžia
+         * @param string $iki - Datų intervalo pabaiga
+         * @param string $separator - (Optional, default="-") Skirtukas tarp datų.
+         * @param bool $full_dates
+         * @return string
+         */
+        public static function fullDateRange( $nuo, $iki, $separator = '-', $full_dates = false ) {
+
+            if ( date( 'Y-m-d', strtotime( $nuo ) ) == date( 'Y-m-d', strtotime( $iki ) ) ) {
+
+                return LithuanianDates::fullDate( $nuo );
+            } elseif ( ( date( 'Y-m', strtotime( $nuo ) ) == date( 'Y-m', strtotime( $iki ) ) ) && ( !$full_dates ) ) {
+
+                return mb_substr( LithuanianDates::fullDate( $nuo ), 0, -3 ) . ' ' . $separator . ' ' . date( 'j', strtotime( $iki ) ) . ' d.';
+            } elseif ( ( date( 'Y', strtotime( $nuo ) ) == date( 'Y', strtotime( $iki ) ) ) && ( !$full_dates ) ) {
+
+                $m = date( 'n', strtotime( $iki ) ) - 1;
+                $d = date( 'j', strtotime( $iki ) );
+
+                return mb_substr( LithuanianDates::fullDate( $nuo ), 0, -3 ) . ' ' . $separator . ' ' .
+                    LithuanianDates::$months['ko'][ $m ] . ' mėn. ' . $d . ' d.';
+            } else {
+
+                return LithuanianDates::fullDate( $nuo ) . ' ' . $separator . ' ' . LithuanianDates::fullDate( $iki );
+            }
+        }
+        
+        /**
+         * Pilnu tekstu išvedama data, pvz.: "2010 m. Sausio 1 d."
+         *
+         * @param string $date
+         * @return string
+         */
+        public static function fullDate( $date ) {
+
+            $ret = '';
+            
+            if ( preg_match( '/([0-9]{4})-([0-9]{2})-([0-9]{2})/', $date, $regs ) ) {
+            
+                $ret = $regs[1] . ' m. ' . LithuanianDates::$months['ko'][ ( $regs[2] - 1 ) ] . ' ' . $regs[3] . ' d.';
+            } elseif ( preg_match( '/([0-9]{2})-([0-9]{2})/', $date, $regs ) ) {
+            
+                $ret = LithuanianDates::$months['ko'][ ( $regs[1] - 1 ) ] . ' ' . $regs[2] . ' d.';
+            } elseif ( preg_match( '/([0-9]{2})/', $date, $regs ) ) {
+            
+                $ret = $regs[1] . ' d.';
+            }
+            
+            return $ret;
+        }
+
+        /**
+         * Sugeneruoja datų pradžios ir pabaigos intervalą, kai paduodama pradžios data ir kiek dienų.
+         * Pvz.: `2019-01-01 - 05` arba: `2019-01-28 - 02-02`
+         *
+         * @param string $date_start - Pradžios data. Formatas: YYYY-MM-DD
+         * @param int $days - Už kiek dienų bus pabaiga.
+         * @return string
+         */
+        public static function dateRangeByDays( $date_start, $days ) {
+
+            if ( ( $days - 1 ) === 0 ) {
+
+                return $date_start;
+            }
+
+            $date_end = date( 'Y-m-d', strtotime( $date_start . ' +' . ( $days - 1 ) . ' DAYS' ) );
+            return self::dateRange( $date_start, $date_end );
+        }
+    }
